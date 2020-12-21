26290e6a42c1f243f72e3a25256c21b44e748907
Thomas <tommyprezioso@gmail.com>
2020-12-21 11:42:10 -0500

    Initial Commit

---
+++ SABarCodeScanner/SABarCodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SABarCodeScannerApp.swift
+//  SABarCodeScanner
+//
+//  Created by Thomas Prezioso Jr on 12/21/20.
+//
+
+import SwiftUI
+
+@main
+struct SABarCodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
