6a5f3010812847ffbcde0589a9c9b930f73369f8
Thomas <tommyprezioso@gmail.com>
2020-12-23 17:21:22 -0500

    first commit

---
+++ SABarCodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,33 @@
+//
+//  BarcodeScannerView.swift
+//  SABarCodeScanner
+//
+//  Created by Thomas Prezioso Jr on 12/21/20.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Rectangle()
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                Spacer().frame(height: 60)
+                Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                Text("Not yet scanned")
+                    .fontWeight(.bold)
+                    .font(.largeTitle)
+                    .foregroundColor(.green)
+                    .padding()
+            }.navigationTitle("Barcode Scanner")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        BarcodeScannerView()
+    }
+}
--- SABarCodeScanner/SABarCodeScannerApp.swift
+++ SABarCodeScanner/SABarCodeScannerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SABarCodeScannerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BarcodeScannerView()
         }
     }
 }
