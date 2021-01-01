05e053acf9ec9543a852dab745c07492602abf84
Thomas <tommyprezioso@gmail.com>
2021-01-01 18:00:40 -0500

    added scanner view and minor fixes

--- SABarCodeScanner/ScannerVC.swift
+++ SABarCodeScanner/ScannerVC.swift
@@ -30,6 +30,21 @@ final class ScannerVC: UIViewController {
     
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCaptureSession()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        guard let previewLayer = previewLayer else {
+            scannerDelegate?.didSurface(error: .invalidDeviceInput)
+            
+            return
+        }
+        previewLayer.frame = view.layer.bounds
+    }
+    
     func setupCaptureSession() {
         guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
             scannerDelegate?.didSurface(error: .invalidDeviceInput)
@@ -90,6 +105,9 @@ extension ScannerVC: AVCaptureMetadataOutputObjectsDelegate {
             return
         }
         
+//         If we want to stop the scanning constantly
+//        captureSession.stopRunning()
+  
         scannerDelegate?.didFind(barcode: barcode)
     }
 }
---
+++ SABarCodeScanner/ScannerView.swift
@@ -0,0 +1,38 @@
+//
+//  ScannerView.swift
+//  SABarCodeScanner
+//
+//  Created by Thomas Prezioso Jr on 1/1/21.
+//
+
+import SwiftUI
+
+struct ScannerView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> ScannerVC {
+        ScannerVC(scannerDelegate: context.coordinator)
+    }
+    
+    func updateUIViewController(_ uiViewController: ScannerVC, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+    
+    final class Coordinator: NSObject, ScannerVCDelegate {
+        func didFind(barcode: String) {
+            print(barcode)
+        }
+        
+        func didSurface(error: CameraError) {
+            print(error.rawValue)
+        }
+        
+        
+    }
+}
+
+struct ScannerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScannerView()
+    }
+}
