8a013855361f36cf08330e07154a1e01bb259094
Thomas <tommyprezioso@gmail.com>
2021-02-04 17:14:56 -0500

    final clean up

--- SABarCodeScanner/Screens/BarcodeScanner/BarcodeScannerView.swift
+++ SABarCodeScanner/Screens/BarcodeScanner/BarcodeScannerView.swift
@@ -17,7 +17,7 @@ struct BarcodeScannerView: View {
         NavigationView {
             VStack {
                 ScannerView(scannedCode: $viewModel.scannedCode, alertItem: $viewModel.alertItem)
-                    .frame(maxWidth: .infinity, maxHeight: 300)
+                    .frame(maxHeight: 300)
                 Spacer().frame(height: 60)
                 Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
                     .font(.title)
@@ -29,7 +29,7 @@ struct BarcodeScannerView: View {
                 
             }.navigationTitle("Barcode Scanner")
             .alert(item: $viewModel.alertItem) { alertItem in
-                Alert(title: Text(alertItem.title), message: Text(alertItem.message), dismissButton: alertItem.alertButton)
+                Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.alertButton)
             }
         }
     }
--- SABarCodeScanner/Utilities/Alert.swift
+++ SABarCodeScanner/Utilities/Alert.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct AlertItem: Identifiable {
     let id = UUID()
-    let title: String
-    let message: String
+    let title: Text
+    let message: Text
     let alertButton: Alert.Button
 }
 
 struct AlertContext {
-    static let invaildDeviceInput = AlertItem(title: "Invaild Device Input", message: "Something is Wrong with the Camera. We are Unable to Caputure the Input", alertButton: .default(Text("OK")))
+    static let invaildDeviceInput = AlertItem(title: Text("Invaild Device Input"), message: Text("Something is Wrong with the Camera. We are Unable to Caputure the Input"), alertButton: .default(Text("OK")))
 
-    static let invaildScannedType = AlertItem(title: "Invaild Scan Type", message: "The Value Scanned is Not Valid. This App Scans Only EAN-8 and EAN-13 Barcodes", alertButton: .default(Text("OK")))
+    static let invaildScannedType = AlertItem(title: Text("Invaild Scan Type"), message: Text("The Value Scanned is Not Valid. This App Scans Only EAN-8 and EAN-13 Barcodes"), alertButton: .default(Text("OK")))
 }
